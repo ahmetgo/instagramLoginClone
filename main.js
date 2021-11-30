a6ed752037f8bfbdc20a387db3d8358dfec1f7cb
ahmetgo <64355476+ahmetgo@users.noreply.github.com>
2021-11-30 18:31:47 +0300

    third commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const photo = document.getElementsByClassName("photo");
 var index = 0;
 showSlide();
 
+
 //fotoğraf değiştirme
 function showSlide(){
     for (var i = 0; i < photo.length; i++) {
@@ -19,13 +20,34 @@ function showSlide(){
 }
 
 
+
 //form işlemleri
+
 const inputPassword = document.getElementById('password');
+const inputMail = document.getElementById('mail');
+const showPassButton = document.querySelector('.show-password');
+const button = document.querySelector('.button');
+
+
+inputMail.oninput=function (){
+    inputMail.setAttribute("value",inputMail.value);
+}
+
+inputPassword.oninput=function (){
+    inputPassword.setAttribute("value",inputPassword.value);
+    
+    if(inputPassword.value.length > 0){
+        showPassButton.style.display = "block";
+    }else{
+        showPassButton.style.display = "none";
+    }
+}
+
 
 const showPassword = () =>{
     inputPassword.type = (inputPassword.type == 'password' ? 'text':'password');
+    showPassButton.innerHTML = (showPassButton.innerHTML == "Göster" ? "Gizle":"Göster");
 }
-
 
 
 
